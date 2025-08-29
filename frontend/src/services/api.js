302898e06f0729c29de94e7bfab92811d464cc10
Mulugeta Linger <37406150+mullersoft@users.noreply.github.com>
2025-08-29 21:27:41 +0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
+  baseURL: "https://smart-payroll-api.onrender.com/api" || "http://127.0.0.1:8000/api",
   headers: {
     Accept: "application/json",
   },
