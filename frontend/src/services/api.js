d5f05668161bde95e48d60601af4f99c97bd9019
mullersoft <mulerselinger@gmail.com>
2025-09-12 20:01:10 +0300

    ok

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  // "https://smart-payroll-api.onrender.com/api" ||
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://smart-payroll-api.onrender.com/api",
+
+  // baseURL: import.meta.env.VITE_API_URL,
   headers: {
     Accept: "application/json",
   },
